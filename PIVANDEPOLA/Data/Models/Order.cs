7be42f2087363433a1ad806637ee286c03f6d368
Александр Гришков <c4grishkovv@mail.ru>
2021-06-13 03:06:53 +0300

    Добавьте файлы проекта.

---
+++ PIVANDEPOLA/Data/Models/Order.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace PIVANDEPOLA.Data.Models
+{
+    public class Order
+    {
+        [BindNever]
+        
+        public int id { get; set; }
+
+        [Display(Name ="Введите имя " )]
+        [StringLength(10)]
+        [Required(ErrorMessage ="Длина имени не менее 5 символов")]
+        public string name { get; set; }
+        [Display(Name = "Введите фамилию ")]
+        [StringLength(10)]
+        [Required(ErrorMessage = "Длина фамилии не менее 5 символов")]
+        public string surname { get; set; }
+        [Display(Name = "Введите адрес ")]
+        [StringLength(10)]
+        [Required(ErrorMessage = "Длина адреса не менее 5 символов")]
+        public string adress { get; set; }
+        [Display(Name = "Введите номер телефона ")]
+        [DataType(DataType.PhoneNumber)]
+        [StringLength(10)]
+        [Required(ErrorMessage = "Длина номера не менее 5 символов")]
+        public string phone { get; set; }
+        [Display(Name = "Введите email ")]
+        [DataType(DataType.EmailAddress)]
+
+        [Required(ErrorMessage = "Длина email не менее 5 символов")]
+        public string email { get; set; }
+        [BindNever]
+        [ScaffoldColumn(false)]
+        public DateTime OrderTime { get; set; }
+
+        public List<OderDetails> oderDetails { get; set; }
+    }
+}
+ 
---
+++ PIVANDEPOLA/Data/Repository/OrdersRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PIVANDEPOLA.Data.intefaces;
+using PIVANDEPOLA.Data.Models;
+
+namespace PIVANDEPOLA.Data.Repository
+{
+    public class OrdersRepository : IAllOrders
+    {
+        private readonly AppDBContent appDBContent;
+        private readonly ShopCart shopCart;
+
+        public OrdersRepository(AppDBContent appDBContent,ShopCart shopCart)
+        {
+            this.appDBContent = appDBContent;
+            this.shopCart = shopCart;
+
+        }
+        public void createOrder(Order order)
+        {
+            order.OrderTime = DateTime.Now;
+            appDBContent.Order.Add(order);
+            appDBContent.SaveChanges();
+
+            var items = shopCart.listShopItems;
+
+            foreach (var el in items)
+            {
+                var orderDetail = new OderDetails()
+                {
+                    PiboID = el.pivo.id,
+                    orderID = order.id,
+                    price = el.pivo.price
+
+
+
+                };
+                appDBContent.OrderDetails.Add(orderDetail);
+
+            }
+            appDBContent.SaveChanges();
+        }
+    }
+}
